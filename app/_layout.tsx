8d632403dfeae4d904e73857849846855c48bf59
Peter Muthama <muthamapeter79@gmail.com>
2024-08-01 12:21:30 +0300

    removed build

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,12 +5,13 @@ import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 
+
 import { useColorScheme } from '@/hooks/useColorScheme';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
-export default function RootLayout() {
+function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
@@ -35,3 +36,5 @@ export default function RootLayout() {
     </ThemeProvider>
   );
 }
+
+export default RootLayout
