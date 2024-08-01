29cb8f8199796b28dc868d4ab3032bfc0eaf7f5b
Peter Muthama <muthamapeter79@gmail.com>
2024-08-01 12:42:20 +0300

    new build

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -11,7 +11,7 @@ import { useColorScheme } from '@/hooks/useColorScheme';
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
-function RootLayout() {
+export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
@@ -37,4 +37,3 @@ function RootLayout() {
   );
 }
 
-export default RootLayout
